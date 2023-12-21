23081cbd48e109520193d6bc4f158ca57ae83706
Federico <organizacionhiv@gmail.com>
2023-12-21 05:35:33 -0300

    deploy changes

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,8 +1,10 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseApp = firebase.initializeApp({
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
   apiKey: process.env.FIREBASE,
   authDomain: "blog-comunity.firebaseapp.com",
   projectId: "blog-comunity",
@@ -10,9 +12,7 @@ const firebaseApp = firebase.initializeApp({
   messagingSenderId: "489732735024",
   appId: "1:489732735024:web:a1132b40debc0041d82df5",
   measurementId: "G-7M307H1HTL",
-});
+};
 
 // Initialize Firebase
-export const auth = firebaseApp.auth();
-export const db = firebaseApp.firestore();
-export default firebaseApp;
+export const app = initializeApp(firebaseConfig);
