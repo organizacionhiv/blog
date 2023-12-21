9ea5eb713033c43f45c2c233256935bf5871d405
Federico <organizacionhiv@gmail.com>
2023-12-21 04:32:00 -0300

    fixed deploy issues

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -1,19 +1,8 @@
 "use client";
 
-import Image from "next/image";
-import styles from "./writePage.module.css";
-import { useEffect, useState } from "react";
 import "react-quill/dist/quill.bubble.css";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
-import {
-  getStorage,
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
-} from "firebase/storage";
-import { app } from "@/utils/firebase";
-import ReactQuill from "react-quill";
 
 const Chat = () => {
   const { status } = useSession();
