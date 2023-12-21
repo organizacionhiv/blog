eedfb9c3fa504d86cf2de02b8e045b80cf7e0c8d
Federico <organizacionhiv@gmail.com>
2023-12-21 02:22:15 -0300

    added svg file and translated

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -8,15 +8,15 @@ import MenuCategories from "../menuCategories/MenuCategories";
 const Menu = () => {
   return (
     <div className={styles.container}>
-      <h2 className={styles.subtitle}>{"What's hot"}</h2>
-      <h1 className={styles.title}>Most Popular</h1>
-      <MenuPosts withImage={false} />
-      <h2 className={styles.subtitle}>Discover by topic</h2>
-      <h1 className={styles.title}>Categories</h1>
+      <h1 className={styles.title}>Categorias</h1>
       <MenuCategories />
-      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h2 className={styles.subtitle}>{"What's hot"}</h2>
+      <h1 className={styles.title}>Mas Popular</h1>
+      <MenuPosts withImage={false} />
+
+      {/* <h2 className={styles.subtitle}>Chosen by the editor</h2>
       <h1 className={styles.title}>Editors Pick</h1>
-      <MenuPosts withImage={true} />
+      <MenuPosts withImage={true} /> */}
     </div>
   );
 };
