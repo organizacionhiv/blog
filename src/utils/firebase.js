f76fdeedd9250f2bbb14e5dc0e6ad8d234defa9a
Federico <organizacionhiv@gmail.com>
2023-12-21 04:16:46 -0300

    fixed deploy issues

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseApp = firebase.initializeApp({
   apiKey: process.env.FIREBASE,
   authDomain: "blog-comunity.firebaseapp.com",
   projectId: "blog-comunity",
@@ -13,7 +10,9 @@ const firebaseConfig = {
   messagingSenderId: "489732735024",
   appId: "1:489732735024:web:a1132b40debc0041d82df5",
   measurementId: "G-7M307H1HTL",
-};
+});
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const auth = firebaseApp.auth();
+export const db = firebaseApp.firestore();
+export default firebaseApp;
