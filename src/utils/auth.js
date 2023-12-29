4040bb929f930eae9ceb5cd1bef414332806d4d3
Federico <organizacionhiv@gmail.com>
2023-12-29 03:47:18 -0300

    deleted github access

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,5 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import GithubProvider from "next-auth/providers/github";
+
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "./connect";
 import { getServerSession } from "next-auth";
@@ -10,10 +10,6 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
 };
